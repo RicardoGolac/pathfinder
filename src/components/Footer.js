f1e21e18a3f74499939368e006fd3b88525f5d61
Ricardo Golac <rigoba98@hotmail.com>
2020-05-17 16:36:03 -0400

    Added algorithm folder

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+import "../css/Footer.css";
+
+export default class Footer extends Component {
+  render() {
+    return <div className="footer"></div>;
+  }
+}
